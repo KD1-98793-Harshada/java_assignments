dd5443013e6a7d1333ff87affa06eccf8022bb1a
Harshada <harshadabiradar18@gmail.com>
2026-09-17 21:11:59 +0530

    Java assignment1_2 added

--- src/Assignment1_2.java
+++ src/Assignment1_2.java
@@ -1,28 +1,25 @@
-//Accept 2 double values from User (using Scanner). Check data type. If 
-//arguments are not doubles, supply suitable error message & terminate. 
-//If numbers are double values, print its average. ////hasNextInt();
-
 import java.util.Scanner;
 
 public class Assignment1_2 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		double x = 0;
-		double y = 0;
-		
-		System.out.println("enter a first value");
-		if(sc.hasNextDouble() &&  (!sc.hasNextInt()) ) {
-			x = sc.hasNextDouble();
-			
-		System.out.println("Enter second value");
-		if(sc.hasNextDouble() &&  (!sc.hasNextInt()) ) {
-			y = sc.hasNextDouble();
-		}
-		else {
-			System.out.println("Enter double ");
-		}
-	
-		
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter first value: ");
+	        if (!sc.hasNextDouble()) {
+	            System.out.println("Error");
+	            return; 
+	        }
+	        double x = sc.nextDouble();
+
+	        System.out.print("Enter second value: ");
+	        if (!sc.hasNextDouble()) {
+	            System.out.println("Error");
+	            return;
+	        }
+	        double y = sc.nextDouble();
+
+	        double average = (x + y) / 2;
+	        System.out.println("Average of " + x + " and " + y + " is: " + average);
+	    }
 	}
-}
